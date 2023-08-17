ad8ca10731aa74183312ad4b5a0e941b21d891a9
Kenal1329 <kenal1329@gmail.com>
2023-08-17 20:10:12 +0530

    FirstProject

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,42 @@
-
+import React, {useState} from 'react'
 import './App.css';
 // import About from './Components/About';
 import Form from './Components/Form';
 import Navbar from './Components/Navbar';
+import Alert from './Components/Alert';
+
 
 function App() {
+  const[mode,setMode]= useState("light")
+
+  const[alert,setAlert]=useState(null);
+
+  const showAlert = (message,type) => {
+    setAlert({
+      msg : message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
+ const toggleMode = () => {
+    if(mode === "light"){
+      setMode("dark")
+      document.body.style.backgroundColor = "#00122c"
+      showAlert("Dark Mode Has Been Enable","success")
+    } else {
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+      showAlert("Light Mode Has Been Enable","success")
+    }
+
+  }
   return (
     <>
-<Navbar title="MyApp" />
-<Form heading="Please Enter Your Text For Analysize Below " />
+<Navbar title="MyApp"  mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert} />
+<Form heading="Please Enter Your Text For Analysize Below " mode={mode} showAlert={showAlert}/>
 {/* <About /> */}
     </>
   );
--- my-app/src/Components/Form.js
+++ my-app/src/Components/Form.js
@@ -1,4 +1,4 @@
-import React, {useInsertionEffect, useState} from 'react'
+import React, {useState} from 'react'
 
 
 export default function Form(props) {
@@ -6,10 +6,13 @@ export default function Form(props) {
     const handleUpclick = () => {
         let newtext = text.toUpperCase();
         setText(newtext);
+        props.showAlert("Converted To UpperCase","success")
+        
     }
     const handledownclick = () => {
         let newtext = text.toLowerCase();
         setText(newtext);
+        props.showAlert("Converted To LowerCase","success")
     }
 
     const handleonchange = (event) => {
@@ -18,27 +21,35 @@ export default function Form(props) {
 
     const handleclear = (event) => {
         setText("");
+        props.showAlert("Clear All","success")
+    }
+
+    const handlecopy = () => {
+        let text = document.getElementById("exampleFromControlTextarea1");
+        text.select();
+        navigator.clipboard.writeText(text.value)
+        props.showAlert("Copy All The Text into the ClipBoard","success")
     }
 
 
     return (
     <>
     <div className="container my-3 ">
-    <h1>{props.heading}</h1>
+    <h1 style={{color: props.mode === "dark"?'white':'black'}}>{props.heading}</h1>
   <div className="mb-3">
-   <textarea className="form-control" value={text} onChange={handleonchange} id="exampleFromControlTextarea1" aria-describedby="emailHelp" rows="8" ></textarea>
+   <textarea className="form-control" style={{backgroundColor: props.mode === 'dark'?'grey':'white' , color: props.mode === "dark"?'white':'black'}}  value={text} onChange={handleonchange} id="exampleFromControlTextarea1"  rows="8" ></textarea>
   <button className="btn btn-primary my-3 mx-2" onClick={handleUpclick}>Convert to UpperCase</button>
   <button className="btn btn-primary my-3 mx-1" onClick={handledownclick}>Convert to LowerCase</button>
   <button className="btn btn-primary my-3 mx-1" onClick={handleclear}>ClearAll</button>
+  <button className="btn btn-primary my-3 mx-1" onClick={handlecopy}>CopyAll</button>
   </div>
     </div>
-    <div className="container my-2">
+    <div className="container my-2 " style={{color: props.mode === "dark"?'white':'black'}}>
     <h2>Your Text Summary</h2>
     <p>{text.split(" ").length} words and {text.length} Characters </p>
     <p>{0.008*text.split(" ").length} words Read </p>
     <h2>Preview</h2>
-    <p>{text}</p>
-
+    <p>{text.length>0?text:"Enter something in the textbox above to preview here"}</p>
     </div>
       </>
   )
